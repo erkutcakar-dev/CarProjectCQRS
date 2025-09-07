b11206e2262bd96679663e967782273c189590f2
Erkut ÇAKAR <erkutcakar@gmail.com>
2025-09-07 15:13:22 +0300

    ...

--- CarProjectCQRS/ViewComponents/MainUiViewComponents/FeaturesIconComponentPartial.cs
+++ CarProjectCQRS/ViewComponents/MainUiViewComponents/FeaturesIconComponentPartial.cs
@@ -1,30 +1,21 @@
 ﻿using CarProjectCQRS.CQRSPattern.Handlers.FeatureHandlers;
 using CarProjectCQRS.CQRSPattern.Results.Feature;
-using CarProjectCQRS.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CarProjectCQRS.ViewComponents.MainUiViewComponents
 {
-    public class FeaturesComponentPartial : ViewComponent
+    public class FeaturesIconComponentPartial :ViewComponent
     {
         private readonly GetFeatureQueryHandler _getFeatureQueryHandler;
-
-        public FeaturesComponentPartial(GetFeatureQueryHandler getFeatureQueryHandler)
+        public FeaturesIconComponentPartial(GetFeatureQueryHandler getFeatureQueryHandler)
         {
             _getFeatureQueryHandler = getFeatureQueryHandler;
         }
-
         public async Task<IViewComponentResult> InvokeAsync()
         {
-           
             var values = await _getFeatureQueryHandler.Handle() ?? new List<GetFeatureQueryResult>();
-            var activeValues = values.Where(f => f.IsActive).Take(1).ToList(); 
-
+            var activeValues = values.Where(f => f.IsActive).Take(6).ToList();
             return View(activeValues);
         }
-
     }
 }
